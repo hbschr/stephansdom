08aa22b033a31a38c125dcf17843e1a598a4ed48
Heiko Hübscher <heiko.huebscher@gmail.com>
2024-05-15 21:28:58 +0200

    style: intro style

---
+++ src/style.test.ts
@@ -0,0 +1,114 @@
+import { describe, expect, it } from "bun:test";
+import parseCss from "./css";
+import parseHtml from "./dom";
+import styleTree, { type StyledNode } from "./style";
+
+const parse = (html: string, css: string): StyledNode =>
+  styleTree(parseHtml(html), parseCss(css));
+
+describe("style tree", () => {
+  it("should not match empty selector", () => {
+    expect(parse("<div></div>", "{display:block;}")).toStrictEqual({
+      node: { tagName: "div" },
+      declarations: {},
+    });
+  });
+
+  it("should match tagName", () => {
+    expect(parse("<div></div>", "div{display:block;}")).toStrictEqual({
+      node: { tagName: "div" },
+      declarations: { display: "block" },
+    });
+  });
+
+  it("should ignore non-matching tagName", () => {
+    expect(parse("<div></div>", "span{display:block;}")).toStrictEqual({
+      node: { tagName: "div" },
+      declarations: {},
+    });
+  });
+
+  it("should match id", () => {
+    expect(parse('<div id="foo"></div>', "#foo{display:block;}")).toStrictEqual(
+      {
+        node: { tagName: "div", attributes: { id: "foo" } },
+        declarations: { display: "block" },
+      },
+    );
+  });
+
+  it("should ignore non-matching id", () => {
+    expect(parse('<div id="foo"></div>', "#bar{display:block;}")).toStrictEqual(
+      {
+        node: { tagName: "div", attributes: { id: "foo" } },
+        declarations: {},
+      },
+    );
+  });
+
+  it("should match class", () => {
+    expect(
+      parse('<div class="foo"></div>', ".foo{display:block;}"),
+    ).toStrictEqual({
+      node: { tagName: "div", attributes: { class: "foo" } },
+      declarations: { display: "block" },
+    });
+  });
+
+  it("should ignore non-matching class", () => {
+    expect(
+      parse('<div class="foo"></div>', ".bar{display:block;}"),
+    ).toStrictEqual({
+      node: { tagName: "div", attributes: { class: "foo" } },
+      declarations: {},
+    });
+  });
+
+  it("should match all", () => {
+    expect(
+      parse(
+        '<div id="foo" class="bar baz"></div>',
+        "div#foo.bar.baz{display:block;}",
+      ),
+    ).toMatchObject({
+      declarations: { display: "block" },
+    });
+  });
+
+  it("should overwrite earlier values", () => {
+    expect(
+      parse("<div></div>", "div{display:span;display:block;}"),
+    ).toMatchObject({
+      declarations: { display: "block" },
+    });
+
+    expect(
+      parse("<div></div>", "div{display:span;}div{display:block;}"),
+    ).toMatchObject({
+      declarations: { display: "block" },
+    });
+  });
+
+  it("should respect specificity", () => {
+    expect(
+      parse(
+        '<p id="foo" class="bar"></p>',
+        "#foo{color:red;}.bar{color:green;}p{color:blue;}",
+      ),
+    ).toMatchObject({
+      declarations: { color: "red" },
+    });
+
+    expect(
+      parse('<p id="foo" class="bar"></p>', ".bar{color:green;}p{color:blue;}"),
+    ).toMatchObject({
+      declarations: { color: "green" },
+    });
+
+    expect(
+      parse('<p id="foo" class="bar"></p>', "p{color:blue;}"),
+    ).toMatchObject({
+      declarations: { color: "blue" },
+    });
+  });
+});
---
+++ src/style.ts
@@ -0,0 +1,56 @@
+import {
+  type Declarations,
+  type Rule,
+  type SimpleSelector,
+  type Specificity,
+  compareSpecificity,
+  specificity,
+} from "./css";
+import { type Element, type Node, hasClassName, isElement } from "./dom";
+
+export interface StyledNode {
+  declarations?: Declarations;
+  node: Node;
+  children?: StyledNode[];
+}
+
+const styleTree = (node: Node, rules: Rule[]): StyledNode => {
+  const styleNode: StyledNode = { node };
+  if (isElement(node)) styleNode.declarations = specifiedValues(node, rules);
+  if (isElement(node) && node.children)
+    styleNode.children = node.children.map((child) => styleTree(child, rules));
+  return styleNode;
+};
+
+export default styleTree;
+
+const specifiedValues = (element: Element, rules: Rule[]): Declarations => {
+  let values = {};
+  const matchingRules = matchRules(element, rules);
+  for (const declarations of matchingRules) {
+    values = { ...values, ...declarations };
+  }
+  return values;
+};
+
+const matchRules = (element: Element, rules: Rule[]): Declarations[] => {
+  return rules
+    .flatMap<[Specificity, Declarations]>((rule) =>
+      rule.selectors
+        .filter((selector) => match(element, selector))
+        .map((selector) => [specificity(selector), rule.declarations]),
+    )
+    .sort(([a], [b]) => compareSpecificity(a, b))
+    .map(([_, declarations]) => declarations);
+};
+
+const match = (element: Element, selector: SimpleSelector): boolean =>
+  (selector.tagName === "*" ||
+    selector.tagName === element.tagName ||
+    (!selector.tagName && (!!selector.id || !!selector.classNames))) &&
+  (!selector.id || selector.id === element.attributes?.id) &&
+  (!selector.classNames ||
+    selector.classNames.every(
+      (className) =>
+        element.attributes && hasClassName(element.attributes, className),
+    ));
